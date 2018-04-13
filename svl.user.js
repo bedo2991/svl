d99780042bf1d3d13116d3c7b159fb0512fc6ef8
Francesco Bedini <fra.bdn@gmail.com>
2018-04-13 14:13:23 +0200

    v. 4.5.1
    Fixing typos

--- svl.user.js
+++ svl.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Street Vector Layer
 // @namespace  wme-champs-it
-// @version    4.5
+// @version    4.5.1
 // @description  Adds a vector layer for drawing streets on the Waze Map editor
 // @include    /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
 // @updateURL  http://code.waze.tools/repository/475e72a8-9df5-4a82-928c-7cd78e21e88d.user.js
@@ -1438,7 +1438,7 @@
 
         $labels.append($("<b>Show Alternative Names</b>"));
         $labels.append($("<br>"));
-        $labels.append($("<i>When enabled, maximum 2 ANs that are different from the primary name are show under the street name&nbsp;</i>"));
+        $labels.append($("<i>When enabled, at most 2 ANs that differ from the primary name are shown under the street name&nbsp;</i>"));
         $labels.append($('<input class="prefElement" title="True or False" id="showANs" type="checkbox" ' + (preferences.showANs ? 'checked' : '') + '></input>'));
         $labels.append("<hr>");
 
