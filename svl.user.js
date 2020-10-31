6fd0fa029bf84b47f421a9800d3c4326eca50f24
bedo2991 <fra.bdn@gmail.com>
2020-10-31 15:28:52 +0100

    Make development branch with automatic updates

--- svl.user.js
+++ svl.user.js
@@ -1,16 +1,18 @@
 // ==UserScript==
 // @name       Street Vector Layer
 // @namespace  wme-champs-it
-// @version    4.9.3
+// @version    4.9.3.1
 // @description  Adds a vector layer for drawing streets on the Waze Map editor
 // @include    /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
-// @updateURL  http://code.waze.tools/repository/475e72a8-9df5-4a82-928c-7cd78e21e88d.user.js
+// @downloadURL  https://github.com/bedo2991/svl/raw/development/svl.user.js
 // @supportURL https://www.waze.com/forum/viewtopic.php?f=819&t=149535
 // @require    https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 // @author     bedo2991
 // @grant    GM_setClipboard
 // @copyright  2015+, bedo2991
 // ==/UserScript==
+
+// @updateURL  http://code.waze.tools/repository/475e72a8-9df5-4a82-928c-7cd78e21e88d.user.js
 
 /*jslint browser: true*/
 /*jslint white: true */
