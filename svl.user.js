f7c78803d609de4ab74a86d6b8ed92f7923aceb8
bedo2991 <fra.bdn@gmail.com>
2021-01-17 19:38:18 +0100

    Improve npm and release process

--- svl.user.js
+++ svl.user.js
@@ -1,20 +1,3 @@
-// ==UserScript==
-// @name       Street Vector Layer
-// @namespace  wme-champs-it
-// @version    5.1.2
-// @description  Adds a vector layer for drawing streets on the Waze Map editor
-// @include    /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
-// @updateURL  http://code.waze.tools/repository/475e72a8-9df5-4a82-928c-7cd78e21e88d.user.js
-// @supportURL https://www.waze.com/forum/viewtopic.php?f=819&t=149535
-// @require    https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
-// @icon       https://raw.githubusercontent.com/bedo2991/svl/master/logo_noText.png
-// @author     bedo2991
-// @grant    GM_setClipboard
-// @copyright  2015+, bedo2991
-// ==/UserScript==
-
-// Code minifier: https://closure-compiler.appspot.com/home
-
 (function svl() {
   /** @type {string} */
   const SVL_VERSION = GM_info.script.version;
