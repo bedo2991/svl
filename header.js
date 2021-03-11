a0409cb23412aa7f3384e410c3903d209a361d95
bedo2991 <fra.bdn@gmail.com>
2021-03-11 19:05:06 +0100

    Fix user.js extension for release

--- header.js
+++ header.js
@@ -4,7 +4,7 @@
 // @version    5.1.4
 // @description  Adds a vector layer for drawing streets on the Waze Map editor
 // @include    /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
-// @updateURL	 https://github.com/bedo2991/svl/releases/latest/download/release.js
+// @updateURL	 https://github.com/bedo2991/svl/releases/latest/download/release.user.js
 // @supportURL https://www.waze.com/forum/viewtopic.php?f=819&t=149535
 // @require    https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 // @icon       https://raw.githubusercontent.com/bedo2991/svl/master/logo_noText.png
