8e28bf59bab46e8281135d660d1e861efb74c82e
bedo2991 <fra.bdn@gmail.com>
2022-10-10 21:14:20 +0200

    Update npm packages, add downloadURL

--- header.js
+++ header.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name       Street Vector Layer
 // @namespace  wme-champs-it
-// @version    5.3.3
+// @version    5.3.4
 // @description  Adds a vector layer for drawing streets on the Waze Map editor
 // @include    /^https:\/\/(www|beta)\.waze\.com(\/\w{2,3}|\/\w{2,3}-\w{2,3}|\/\w{2,3}-\w{2,3}-\w{2,3})?\/editor\b/
 // @updateURL	 https://github.com/bedo2991/svl/releases/latest/download/release.user.js
+// @downloadURL https://github.com/bedo2991/svl/releases/latest/download/release.user.js
 // @supportURL https://www.waze.com/forum/viewtopic.php?f=819&t=149535
 // @require    https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
 // @icon       https://raw.githubusercontent.com/bedo2991/svl/master/logo_noText.png
